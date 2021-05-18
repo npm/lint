b0e090192458ebfd8712dd1ca3196d051bb5591c
isaacs <i@izs.me>
2021-05-18 14:55:12 -0700

    tests

---
+++ tap-snapshots/test/index.js.test.cjs
@@ -0,0 +1,46 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/index.js --fix TAP test linting with args > called spawn 1`] = `
+Array [
+  "{CWD}/node_modules/.bin/eslint",
+  Array [
+    "-c",
+    "{CWD}/lib/.eslintrc.json",
+    "--fix",
+  ],
+  Object {
+    "stdio": "inherit",
+  },
+]
+`
+
+exports[`test/index.js TAP test linting > called spawn 1`] = `
+Array [
+  "{CWD}/node_modules/.bin/eslint",
+  Array [
+    "-c",
+    "{CWD}/lib/.eslintrc.json",
+  ],
+  Object {
+    "stdio": "inherit",
+  },
+]
+`
+
+exports[`test/index.js TAP test linting with exit code > called spawn 1`] = `
+Array [
+  "{CWD}/node_modules/.bin/eslint",
+  Array [
+    "-c",
+    "{CWD}/lib/.eslintrc.json",
+  ],
+  Object {
+    "stdio": "inherit",
+  },
+]
+`
---
+++ test/index.js
@@ -0,0 +1,62 @@
+const t = require('tap')
+
+const cwd = require('path').resolve(__dirname, '..')
+t.formatSnapshot = obj => {
+  if (Array.isArray(obj))
+    return obj.map(o => t.formatSnapshot(o))
+  if (typeof obj === 'string')
+    return obj.split(cwd).join('{CWD}').replace(/\\/g, '/')
+  if (obj && typeof obj === 'object') {
+    return Object.entries(obj)
+      .map(([k, v]) => [k, t.formatSnapshot(v)])
+      .reduce((o, [k, v]) => {
+        o[k] = v
+        return o
+      }, {})
+  }
+  return obj
+}
+
+let SPAWN_CALLED = null
+let SPAWN_EXITCODE = 0
+
+const runIndex = (t, ...args) => {
+  process.argv = [process.argv[0], process.argv[1], ...args]
+  const {exitCode} = process
+  t.teardown(() => {
+    if (t.passing())
+      process.exitCode = exitCode
+    else
+      process.exitCode = 1
+  })
+  t.mock('../lib/index.js', {
+    child_process: {
+      spawnSync: (cmd, args, options) => {
+        SPAWN_CALLED = [cmd, args, options]
+        return { exitCode: SPAWN_EXITCODE }
+      },
+    },
+  })
+}
+
+t.test('test linting', t => {
+  runIndex(t)
+  t.matchSnapshot(SPAWN_CALLED, 'called spawn')
+  t.equal(process.exitCode, 0, 'exited with 0 status code')
+  t.end()
+})
+
+t.test('test linting with args', t => {
+  runIndex(t, '--fix')
+  t.matchSnapshot(SPAWN_CALLED, 'called spawn')
+  t.equal(process.exitCode, 0, 'exited with 0 status code')
+  t.end()
+})
+
+t.test('test linting with exit code', t => {
+  SPAWN_EXITCODE = 1
+  runIndex(t)
+  t.matchSnapshot(SPAWN_CALLED, 'called spawn')
+  t.equal(process.exitCode, 1, 'exited with 1 status code')
+  t.end()
+})
